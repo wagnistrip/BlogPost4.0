822791bfecb8b627ff01d586aa92813debc99bcb
wagnistrip <developer@wagnistrip.com>
2025-01-26 17:00:50 +0530

    ggg

--- resources/views/Frontend/Blogs/detail.blade.php
+++ resources/views/Frontend/Blogs/detail.blade.php
@@ -73,30 +73,33 @@
                         <h4 class="text-uppercase mb-4" style="letter-spacing: 5px;">Categories</h4>
                         <div class="bg-white" style="padding: 30px;">
                             <ul class="list-inline m-0">
-                                <li class="mb-3 d-flex justify-content-between align-items-center">
-                                    <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Web
-                                        Design</a>
+                                <li class="mb-2 d-flex justify-content-between align-items-center">
+                                    <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Flights</a>
                                     <span class="badge badge-primary badge-pill">150</span>
                                 </li>
-                                <li class="mb-3 d-flex justify-content-between align-items-center">
-                                    <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Web
-                                        Development</a>
+                                <li class="mb-2 d-flex justify-content-between align-items-center">
+                                    <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Hotels</a>
                                     <span class="badge badge-primary badge-pill">131</span>
                                 </li>
-                                <li class="mb-3 d-flex justify-content-between align-items-center">
+                                <li class="mb-2 d-flex justify-content-between align-items-center">
                                     <a class="text-dark" href="#"><i
-                                            class="fa fa-angle-right text-primary mr-2"></i>Online Marketing</a>
+                                            class="fa fa-angle-right text-primary mr-2"></i>Tours</a>
                                     <span class="badge badge-primary badge-pill">78</span>
                                 </li>
-                                <li class="mb-3 d-flex justify-content-between align-items-center">
+                                <li class="mb-2 d-flex justify-content-between align-items-center">
                                     <a class="text-dark" href="#"><i
-                                            class="fa fa-angle-right text-primary mr-2"></i>Keyword Research</a>
+                                            class="fa fa-angle-right text-primary mr-2"></i>Packages</a>
                                     <span class="badge badge-primary badge-pill">56</span>
+                                </li>
+                                <li class="mb-2 d-flex justify-content-between align-items-center">
+                                    <a class="text-dark" href="#"><i
+                                            class="fa fa-angle-right text-primary mr-2"></i>Buses</a>
+                                    <span class="badge badge-primary badge-pill">98</span>
                                 </li>
                                 <li class="d-flex justify-content-between align-items-center">
                                     <a class="text-dark" href="#"><i
-                                            class="fa fa-angle-right text-primary mr-2"></i>Email Marketing</a>
-                                    <span class="badge badge-primary badge-pill">98</span>
+                                            class="fa fa-angle-right text-primary mr-2"></i>Cabs</a>
+                                    <span class="badge badge-primary badge-pill">08</span>
                                 </li>
                             </ul>
                         </div>
