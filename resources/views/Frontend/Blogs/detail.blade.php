913f418096e49e3a30eb9fe79d4bb507e3dcd80c
wagnistrip <developer@wagnistrip.com>
2025-01-26 17:34:01 +0530

    ggg

--- resources/views/Frontend/Blogs/detail.blade.php
+++ resources/views/Frontend/Blogs/detail.blade.php
@@ -1,138 +1,138 @@
 @extends('frontend.layout.master')
 
 @section('content')
-    <div class="container-fluid py-5">
-        <div class="container py-5">
-            <div class="row">
-                <div class="col-lg-8">
-                    <!-- Blog Detail Start -->
-                    <div class="pb-3">
-                        <div class="blog-item">
-                            <div class="position-relative">
-                                <img class="img-fluid w-100" src="{{ asset('img/blog-1.jpg') }}" alt="">
-                                <div class="blog-date">
-                                    <h6 class="font-weight-bold mb-n1">01</h6>
-                                    <small class="text-white text-uppercase">Jan</small>
-                                </div>
+<div class="container-fluid py-5">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-8">
+                <!-- Blog Detail Start -->
+                <div class="pb-3">
+                    <div class="blog-item">
+                        <div class="position-relative">
+                            <img class="img-fluid w-100" src="{{ asset('img/blog-1.jpg') }}" alt="">
+                            <div class="blog-date">
+                                <h6 class="font-weight-bold mb-n1">01</h6>
+                                <small class="text-white text-uppercase">Jan</small>
                             </div>
-                        </div>
-                        <div class="bg-white mb-3" style="padding: 30px;">
-                            <div class="d-flex mb-3">
-                                <a class="text-primary text-uppercase text-decoration-none" href="">Admin</a>
-                                <span class="text-primary px-2">|</span>
-                                <a class="text-primary text-uppercase text-decoration-none" href="">Tours & Travel</a>
-                            </div>
-                            <h2 class="mb-3">Dolor justo sea kasd lorem clita justo diam amet</h2>
-                            <p>Sadipscing labore amet rebum est et justo gubergren. Et eirmod ipsum sit diam ut
-                                magna lorem. Nonumy vero labore lorem sanctus rebum et lorem magna kasd, stet
-                                amet magna accusam consetetur eirmod. Kasd accusam sit ipsum sadipscing et at at
-                                sanctus et. Ipsum sit gubergren dolores et, consetetur justo invidunt at et
-                                aliquyam ut et vero clita. Diam sea sea no sed dolores diam nonumy, gubergren
-                                sit stet no diam kasd vero.</p>
-                            <p>Voluptua est takimata stet invidunt sed rebum nonumy stet, clita aliquyam dolores
-                                vero stet consetetur elitr takimata rebum sanctus. Sit sed accusam stet sit
-                                nonumy kasd diam dolores, sanctus lorem kasd duo dolor dolor vero sit et. Labore
-                                ipsum duo sanctus amet eos et. Consetetur no sed et aliquyam ipsum justo et,
-                                clita lorem sit vero amet amet est dolor elitr, stet et no diam sit. Dolor erat
-                                justo dolore sit invidunt.</p>
-                            <h4 class="mb-3">Est dolor lorem et ea</h4>
-                            <img class="img-fluid w-50 float-left mr-4 mb-2" src="{{ asset('img/blog-2.jpg') }}">
-                            <p>Diam dolor est labore duo invidunt ipsum clita et, sed et lorem voluptua tempor
-                                invidunt at est sanctus sanctus. Clita dolores sit kasd diam takimata justo diam
-                                lorem sed. Magna amet sed rebum eos. Clita no magna no dolor erat diam tempor
-                                rebum consetetur, sanctus labore sed nonumy diam lorem amet eirmod. No at tempor
-                                sea diam kasd, takimata ea nonumy elitr sadipscing gubergren erat. Gubergren at
-                                lorem invidunt sadipscing rebum sit amet ut ut, voluptua diam dolores at
-                                sadipscing stet. Clita dolor amet dolor ipsum vero ea ea eos. Invidunt sed diam
-                                dolores takimata dolor dolore dolore sit. Sit ipsum erat amet lorem et, magna
-                                sea at sed et eos. Accusam eirmod kasd lorem clita sanctus ut consetetur et. Et
-                                duo tempor sea kasd clita ipsum et.</p>
-                            <h5 class="mb-3">Est dolor lorem et ea</h5>
-                            <img class="img-fluid w-50 float-right ml-4 mb-2" src="{{ asset('img/blog-3.jpg') }}">
-                            <p>Diam dolor est labore duo invidunt ipsum clita et, sed et lorem voluptua tempor
-                                invidunt at est sanctus sanctus. Clita dolores sit kasd diam takimata justo diam
-                                lorem sed. Magna amet sed rebum eos. Clita no magna no dolor erat diam tempor
-                                rebum consetetur, sanctus labore sed nonumy diam lorem amet eirmod. No at tempor
-                                sea diam kasd, takimata ea nonumy elitr sadipscing gubergren erat. Gubergren at
-                                lorem invidunt sadipscing rebum sit amet ut ut, voluptua diam dolores at
-                                sadipscing stet. Clita dolor amet dolor ipsum vero ea ea eos. Invidunt sed diam
-                                dolores takimata dolor dolore dolore sit. Sit ipsum erat amet lorem et, magna
-                                sea at sed et eos. Accusam eirmod kasd lorem clita sanctus ut consetetur et. Et
-                                duo tempor sea kasd clita ipsum et. Takimata kasd diam justo est eos erat
-                                aliquyam et ut.</p>
                         </div>
                     </div>
-                    <!-- Blog Detail End -->
+                    <div class="bg-white mb-3" style="padding: 30px;">
+                        <div class="d-flex mb-3">
+                            <a class="text-primary text-uppercase text-decoration-none" href="">Admin</a>
+                            <span class="text-primary px-2">|</span>
+                            <a class="text-primary text-uppercase text-decoration-none" href="">Tours & Travel</a>
+                        </div>
+                        <h2 class="mb-3">Dolor justo sea kasd lorem clita justo diam amet</h2>
+                        <p>Sadipscing labore amet rebum est et justo gubergren. Et eirmod ipsum sit diam ut
+                            magna lorem. Nonumy vero labore lorem sanctus rebum et lorem magna kasd, stet
+                            amet magna accusam consetetur eirmod. Kasd accusam sit ipsum sadipscing et at at
+                            sanctus et. Ipsum sit gubergren dolores et, consetetur justo invidunt at et
+                            aliquyam ut et vero clita. Diam sea sea no sed dolores diam nonumy, gubergren
+                            sit stet no diam kasd vero.</p>
+                        <p>Voluptua est takimata stet invidunt sed rebum nonumy stet, clita aliquyam dolores
+                            vero stet consetetur elitr takimata rebum sanctus. Sit sed accusam stet sit
+                            nonumy kasd diam dolores, sanctus lorem kasd duo dolor dolor vero sit et. Labore
+                            ipsum duo sanctus amet eos et. Consetetur no sed et aliquyam ipsum justo et,
+                            clita lorem sit vero amet amet est dolor elitr, stet et no diam sit. Dolor erat
+                            justo dolore sit invidunt.</p>
+                        <h4 class="mb-3">Est dolor lorem et ea</h4>
+                        <img class="img-fluid w-50 float-left mr-4 mb-2" src="{{ asset('img/blog-2.jpg') }}">
+                        <p>Diam dolor est labore duo invidunt ipsum clita et, sed et lorem voluptua tempor
+                            invidunt at est sanctus sanctus. Clita dolores sit kasd diam takimata justo diam
+                            lorem sed. Magna amet sed rebum eos. Clita no magna no dolor erat diam tempor
+                            rebum consetetur, sanctus labore sed nonumy diam lorem amet eirmod. No at tempor
+                            sea diam kasd, takimata ea nonumy elitr sadipscing gubergren erat. Gubergren at
+                            lorem invidunt sadipscing rebum sit amet ut ut, voluptua diam dolores at
+                            sadipscing stet. Clita dolor amet dolor ipsum vero ea ea eos. Invidunt sed diam
+                            dolores takimata dolor dolore dolore sit. Sit ipsum erat amet lorem et, magna
+                            sea at sed et eos. Accusam eirmod kasd lorem clita sanctus ut consetetur et. Et
+                            duo tempor sea kasd clita ipsum et.</p>
+                        <h5 class="mb-3">Est dolor lorem et ea</h5>
+                        <img class="img-fluid w-50 float-right ml-4 mb-2" src="{{ asset('img/blog-3.jpg') }}">
+                        <p>Diam dolor est labore duo invidunt ipsum clita et, sed et lorem voluptua tempor
+                            invidunt at est sanctus sanctus. Clita dolores sit kasd diam takimata justo diam
+                            lorem sed. Magna amet sed rebum eos. Clita no magna no dolor erat diam tempor
+                            rebum consetetur, sanctus labore sed nonumy diam lorem amet eirmod. No at tempor
+                            sea diam kasd, takimata ea nonumy elitr sadipscing gubergren erat. Gubergren at
+                            lorem invidunt sadipscing rebum sit amet ut ut, voluptua diam dolores at
+                            sadipscing stet. Clita dolor amet dolor ipsum vero ea ea eos. Invidunt sed diam
+                            dolores takimata dolor dolore dolore sit. Sit ipsum erat amet lorem et, magna
+                            sea at sed et eos. Accusam eirmod kasd lorem clita sanctus ut consetetur et. Et
+                            duo tempor sea kasd clita ipsum et. Takimata kasd diam justo est eos erat
+                            aliquyam et ut.</p>
+                    </div>
+                </div>
+                <!-- Blog Detail End -->
+            </div>
+
+            <div class="col-lg-4 mt-5 mt-lg-0">
+
+
+                <!-- Category List -->
+                <div class="mb-5">
+                    <h4 class="text-uppercase mb-4" style="letter-spacing: 5px;">Categories</h4>
+                    <div class="bg-white" style="padding: 30px;">
+                        <ul class="list-inline m-0">
+                            <li class="mb-2 d-flex justify-content-between align-items-center">
+                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Flights</a>
+                                <span class="badge badge-primary badge-pill">150</span>
+                            </li>
+                            <li class="mb-2 d-flex justify-content-between align-items-center">
+                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Hotels</a>
+                                <span class="badge badge-primary badge-pill">131</span>
+                            </li>
+                            <li class="mb-2 d-flex justify-content-between align-items-center">
+                                <a class="text-dark" href="#"><i
+                                        class="fa fa-angle-right text-primary mr-2"></i>Tours</a>
+                                <span class="badge badge-primary badge-pill">78</span>
+                            </li>
+                            <li class="mb-2 d-flex justify-content-between align-items-center">
+                                <a class="text-dark" href="#"><i
+                                        class="fa fa-angle-right text-primary mr-2"></i>Packages</a>
+                                <span class="badge badge-primary badge-pill">56</span>
+                            </li>
+                            <li class="mb-2 d-flex justify-content-between align-items-center">
+                                <a class="text-dark" href="#"><i
+                                        class="fa fa-angle-right text-primary mr-2"></i>Buses</a>
+                                <span class="badge badge-primary badge-pill">98</span>
+                            </li>
+                            <li class="d-flex justify-content-between align-items-center">
+                                <a class="text-dark" href="#"><i
+                                        class="fa fa-angle-right text-primary mr-2"></i>Cabs</a>
+                                <span class="badge badge-primary badge-pill">08</span>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
 
-                <div class="col-lg-4 mt-5 mt-lg-0">
-
-
-                    <!-- Category List -->
-                    <div class="mb-5">
-                        <h4 class="text-uppercase mb-4" style="letter-spacing: 5px;">Categories</h4>
-                        <div class="bg-white" style="padding: 30px;">
-                            <ul class="list-inline m-0">
-                                <li class="mb-2 d-flex justify-content-between align-items-center">
-                                    <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Flights</a>
-                                    <span class="badge badge-primary badge-pill">150</span>
-                                </li>
-                                <li class="mb-2 d-flex justify-content-between align-items-center">
-                                    <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Hotels</a>
-                                    <span class="badge badge-primary badge-pill">131</span>
-                                </li>
-                                <li class="mb-2 d-flex justify-content-between align-items-center">
-                                    <a class="text-dark" href="#"><i
-                                            class="fa fa-angle-right text-primary mr-2"></i>Tours</a>
-                                    <span class="badge badge-primary badge-pill">78</span>
-                                </li>
-                                <li class="mb-2 d-flex justify-content-between align-items-center">
-                                    <a class="text-dark" href="#"><i
-                                            class="fa fa-angle-right text-primary mr-2"></i>Packages</a>
-                                    <span class="badge badge-primary badge-pill">56</span>
-                                </li>
-                                <li class="mb-2 d-flex justify-content-between align-items-center">
-                                    <a class="text-dark" href="#"><i
-                                            class="fa fa-angle-right text-primary mr-2"></i>Buses</a>
-                                    <span class="badge badge-primary badge-pill">98</span>
-                                </li>
-                                <li class="d-flex justify-content-between align-items-center">
-                                    <a class="text-dark" href="#"><i
-                                            class="fa fa-angle-right text-primary mr-2"></i>Cabs</a>
-                                    <span class="badge badge-primary badge-pill">08</span>
-                                </li>
-                            </ul>
+                <!-- Recent Post -->
+                <div class="mb-5">
+                    <h4 class="text-uppercase mb-4" style="letter-spacing: 5px;">Recent Post</h4>
+                    <a class="d-flex align-items-center text-decoration-none bg-white mb-3" href="">
+                        <img class="img-fluid" src="{{ asset('img/blog-100x100.jpg')}}" alt="">
+                        <div class="pl-3">
+                            <h6 class="m-1">Diam lorem dolore justo eirmod lorem dolore</h6>
+                            <small>Jan 01, 2050</small>
                         </div>
-                    </div>
-
-                    <!-- Recent Post -->
-                    <div class="mb-5">
-                        <h4 class="text-uppercase mb-4" style="letter-spacing: 5px;">Recent Post</h4>
-                        <a class="d-flex align-items-center text-decoration-none bg-white mb-3" href="">
-                            <img class="img-fluid" src="{{ asset('img/blog-100x100.jpg')}}" alt="">
-                            <div class="pl-3">
-                                <h6 class="m-1">Diam lorem dolore justo eirmod lorem dolore</h6>
-                                <small>Jan 01, 2050</small>
-                            </div>
-                        </a>
-                        <a class="d-flex align-items-center text-decoration-none bg-white mb-3" href="">
-                            <img class="img-fluid" src="{{ asset('img/blog-100x100.jpg')}}" alt="">
-                            <div class="pl-3">
-                                <h6 class="m-1">Diam lorem dolore justo eirmod lorem dolore</h6>
-                                <small>Jan 01, 2050</small>
-                            </div>
-                        </a>
-                        <a class="d-flex align-items-center text-decoration-none bg-white mb-3" href="">
-                            <img class="img-fluid" src="{{ asset('img/blog-100x100.jpg')}}" alt="">
-                            <div class="pl-3">
-                                <h6 class="m-1">Diam lorem dolore justo eirmod lorem dolore</h6>
-                                <small>Jan 01, 2050</small>
-                            </div>
-                        </a>
-                    </div>
-                 
+                    </a>
+                    <a class="d-flex align-items-center text-decoration-none bg-white mb-3" href="">
+                        <img class="img-fluid" src="{{ asset('img/blog-100x100.jpg')}}" alt="">
+                        <div class="pl-3">
+                            <h6 class="m-1">Diam lorem dolore justo eirmod lorem dolore</h6>
+                            <small>Jan 01, 2050</small>
+                        </div>
+                    </a>
+                    <a class="d-flex align-items-center text-decoration-none bg-white mb-3" href="">
+                        <img class="img-fluid" src="{{ asset('img/blog-100x100.jpg')}}" alt="">
+                        <div class="pl-3">
+                            <h6 class="m-1">Diam lorem dolore justo eirmod lorem dolore</h6>
+                            <small>Jan 01, 2050</small>
+                        </div>
+                    </a>
                 </div>
+
             </div>
         </div>
     </div>
+</div>
 @endsection
--- resources/views/Frontend/layout/master.blade.php
+++ resources/views/Frontend/layout/master.blade.php
@@ -16,30 +16,31 @@
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     @yield('head')
 </head>
+
 <body>
-     @include('frontend.include.topBar')
-     @include('frontend.include.navBar')
-     @include('frontend.include.header')
+    @include('frontend.include.topBar')
+    @include('frontend.include.navBar')
+    @include('frontend.include.header')
 
-        @yield('content')
+    @yield('content')
 
-     @include('frontend.include.footer')
+    @include('frontend.include.footer')
 
 
 
-<a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top">
-    <i class="fa fa-angle-double-up"></i>
-</a>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-<script src="{{ asset('lib/easing/easing.min.js') }}"></script>
-<script src="{{ asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
-<script src="{{ asset('lib/tempusdominus/js/moment.min.js') }}"></script>
-<script src="{{ asset('lib/tempusdominus/js/moment-timezone.min.js') }}"></script>
-<script src="{{ asset('lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script>
-<script src="{{ asset('mail/jqBootstrapValidation.min.js') }}"></script>
-<script src="{{ asset('mail/contact.js') }}"></script>
-<script src="{{ asset('js/main.js') }}"></script>
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top">
+        <i class="fa fa-angle-double-up"></i>
+    </a>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('lib/easing/easing.min.js') }}"></script>
+    <script src="{{ asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('lib/tempusdominus/js/moment.min.js') }}"></script>
+    <script src="{{ asset('lib/tempusdominus/js/moment-timezone.min.js') }}"></script>
+    <script src="{{ asset('lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+    <script src="{{ asset('mail/jqBootstrapValidation.min.js') }}"></script>
+    <script src="{{ asset('mail/contact.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
 
 </body>
 
